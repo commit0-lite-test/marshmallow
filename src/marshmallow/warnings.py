1e063f061dc98ef5772d3e427181b587c6fc712d
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 13:03:23 -0800

    fix: Add missing newline at the end of the RemovedInMarshmallow4Warning class

--- src/marshmallow/warnings.py
+++ src/marshmallow/warnings.py
@@ -1,10 +1,11 @@
 class RemovedInMarshmallow4Warning(DeprecationWarning):
     """
     Warning class to indicate functionality that will be removed in Marshmallow 4.
-    
+
     This warning is a subclass of DeprecationWarning and is used to notify users
     about features or behaviors that are deprecated and will be removed in the
     next major version (Marshmallow 4) of the library.
     """
+
     def __init__(self, message):
         super().__init__(message)
